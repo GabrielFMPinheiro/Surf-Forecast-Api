60e1f450e236d731efc824ff5d4f4e05dd65f608
Gabriel <g.felippe5965@gmail.com>
2022-07-10 11:07:00 -0300

    feat: Encrypt user password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
 import mongoose, { Document, Schema } from 'mongoose';
-
+import AuthService from '@src/services/auth';
 export interface User {
   _id?: string;
   name: string;
@@ -44,5 +44,18 @@ schema.path('email').validate(
   'already exists in the database.',
   CUSTOM_VALIDATION.DUPLICATED
 );
+
+schema.pre('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (error) {
+    console.error(`Error hashing the password for the user ${this.name}`);
+  }
+});
 
 export const User = mongoose.model<User>('User', schema);
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,4 +1,5 @@
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Users functional tests', () => {
   beforeEach(async () => {
@@ -6,7 +7,7 @@ describe('Users functional tests', () => {
   });
 
   describe('When creating a user', () => {
-    it('should create a user with success', async () => {
+    it('should successfully create a new user with encrypted password', async () => {
       const newUser = {
         name: 'John Doe',
         email: 'john@gmail.com',
@@ -14,7 +15,15 @@ describe('Users functional tests', () => {
       };
       const response = await global.testRequest.post('/users').send(newUser);
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      expect(
+        AuthService.comparePasswords(newUser.password, response.body.password)
+      ).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      );
     });
 
     it('Should return a validation error when a field is missing', async () => {
